72dd61e361f175456d5d520cf6ea3524b0fb8e1d
liuyouren <lyr632537786@163.com>
2018-11-06 11:21:13 +0800

    update

---
+++ src/containers/views/test/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+import { ComponentExt } from "@utils/reactExt"
+
+class Test extends ComponentExt {
+    render() {
+        return (
+            <div>
+                <span>Test</span>
+            </div>
+        )
+    }
+}
+
+export default Test
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import { configure } from "mobx"
 import { Provider } from "mobx-react"
+import { AppContainer } from "react-hot-loader"
 // import './index.scss';
 // 解决模块声明问题 这时候我们在根目录下新建一个typings文件夹，用于存放.scss的模块声明，以及后续需要用到的全局校验接口，然后新建typed-css-modules.d.ts文件用于存放.scss模块声明
 // import * as styles from './index.scss';
@@ -16,7 +17,9 @@ configure({ enforceActions: "observed" })
 const render = () => {
     ReactDOM.render(
         <Provider {...store}>
-            <App><Counter/></App>
+            <App>
+                <Counter />
+            </App>
         </Provider>,
         document.querySelector("#app")
     )
