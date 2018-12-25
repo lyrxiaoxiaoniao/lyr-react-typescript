8bb2a7485992e3d73464f423eeecf4189c01234a
liuyouren <lyr632537786@163.com>
2018-12-25 18:06:40 +0800

    update gulp

---
+++ gulp-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    //部署正式服务器上
+    devDist: {
+        //部署到服务器的路径
+        remotePath: '/www',
+        //ip地址
+        host: '120.77.203.163',
+        //帐号
+        user: 'root',
+        //密码
+        pass: 'xxxxxxx',
+        //端口
+        port: 22
+    },
+    //程序编译好路径
+    publicPath: '/dist/'
+}
---
+++ record/gulp自动化部署.md
@@ -0,0 +1,12 @@
+### 1 gulp 自动化部署
+
+```
+    1. 全局安装 gulp 和 gulp-sftp：
+    $ cnpm install --global gulp
+    $ cnpm install --global gulp-sftp
+
+    2. 作为项目的开发依赖（devDependencies）安装：
+    $ cnpm install --save-dev gulp
+    $ cnpm install --save-dev gulp-sftp
+
+```
