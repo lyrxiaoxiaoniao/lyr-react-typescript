ac924df94111cffa1358551a79d59b52533604df
liuyouren <lyr632537786@163.com>
2019-01-02 17:15:49 +0800

    update

--- src/containers/views/Login/index.scss.d.ts
+++ src/containers/views/Login/index.scss.d.ts
@@ -3,3 +3,4 @@ export const componentsFormDemoNormalLogin: string;
 export const loginForm: string;
 export const loginFormForgot: string;
 export const loginFormButton: string;
+export const loginContainer: string;
--- src/containers/views/Login/index.tsx
+++ src/containers/views/Login/index.tsx
@@ -3,6 +3,7 @@ import { ComponentExt } from "@utils/reactExt"
 import { Form, Icon, Input, Button, Checkbox } from "antd"
 import axios from "axios"
 import "./index.scss"
+import * as style from "./index.scss"
 const FormItem = Form.Item
 class LoginForm extends ComponentExt<any, any> {
     handleSubmit = (e: React.FormEvent) => {
@@ -23,66 +24,68 @@ class LoginForm extends ComponentExt<any, any> {
     render() {
         const { getFieldDecorator } = this.props.form
         return (
-            <Form onSubmit={this.handleSubmit} className="login-form">
-                <FormItem>
-                    {getFieldDecorator("userName", {
-                        rules: [
-                            {
-                                required: true,
-                                message: "Please input your username!"
-                            }
-                        ]
-                    })(
-                        <Input
-                            prefix={
-                                <Icon
-                                    type="user"
-                                    style={{ color: "rgba(0,0,0,.25)" }}
-                                />
-                            }
-                            placeholder="Username"
-                        />
-                    )}
-                </FormItem>
-                <FormItem>
-                    {getFieldDecorator("password", {
-                        rules: [
-                            {
-                                required: true,
-                                message: "Please input your Password!"
-                            }
-                        ]
-                    })(
-                        <Input
-                            prefix={
-                                <Icon
-                                    type="lock"
-                                    style={{ color: "rgba(0,0,0,.25)" }}
-                                />
-                            }
-                            type="password"
-                            placeholder="Password"
-                        />
-                    )}
-                </FormItem>
-                <FormItem>
-                    {getFieldDecorator("remember", {
-                        valuePropName: "checked",
-                        initialValue: true
-                    })(<Checkbox>Remember me</Checkbox>)}
-                    <a className="login-form-forgot" href="">
-                        Forgot password
-                    </a>
-                    <Button
-                        type="primary"
-                        htmlType="submit"
-                        className="login-form-button"
-                    >
-                        Log in
-                    </Button>
-                    Or <a href="">register now!</a>
-                </FormItem>
-            </Form>
+            <div className={style.loginContainer}>
+                <Form onSubmit={this.handleSubmit} className={style.loginForm}>
+                    <FormItem>
+                        {getFieldDecorator("userName", {
+                            rules: [
+                                {
+                                    required: true,
+                                    message: "Please input your username!"
+                                }
+                            ]
+                        })(
+                            <Input
+                                prefix={
+                                    <Icon
+                                        type="user"
+                                        style={{ color: "rgba(0,0,0,.25)" }}
+                                    />
+                                }
+                                placeholder="Username"
+                            />
+                        )}
+                    </FormItem>
+                    <FormItem>
+                        {getFieldDecorator("password", {
+                            rules: [
+                                {
+                                    required: true,
+                                    message: "Please input your Password!"
+                                }
+                            ]
+                        })(
+                            <Input
+                                prefix={
+                                    <Icon
+                                        type="lock"
+                                        style={{ color: "rgba(0,0,0,.25)" }}
+                                    />
+                                }
+                                type="password"
+                                placeholder="Password"
+                            />
+                        )}
+                    </FormItem>
+                    <FormItem>
+                        {getFieldDecorator("remember", {
+                            valuePropName: "checked",
+                            initialValue: true
+                        })(<Checkbox>Remember me</Checkbox>)}
+                        <a className={style.loginFormForgot} href="">
+                            Forgot password
+                        </a>
+                        <Button
+                            type="primary"
+                            htmlType="submit"
+                            className={style.loginFormButton}
+                        >
+                            Log in
+                        </Button>
+                        Or <a href="">register now!</a>
+                    </FormItem>
+                </Form>
+            </div>
         )
     }
 }
