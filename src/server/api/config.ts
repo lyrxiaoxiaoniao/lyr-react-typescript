515b23b4c62e72c8d56209bd2b14bed27cbed143
liuyouren <lyr632537786@163.com>
2019-05-17 18:10:51 +0800

    update

--- src/server/api/config.ts
+++ src/server/api/config.ts
@@ -1,7 +1,4 @@
-const isPro = process.env.APP_ENV === "prod"
-const baseURL = isPro
-    ? "http://localhost:3000/api"
-    : "http://www.liuyouren.top/api"
 export default {
-    baseURL
+    // baseURL: "http://localhost:3000/api"
+    baseURL: "http://www.liuyouren.top/api"
 }
