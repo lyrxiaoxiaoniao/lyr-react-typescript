c9fe9027b3d7070cc297d67c574a0f53b8cea174
liuyouren <lyr632537786@163.com>
2019-06-19 17:46:01 +0800

    update

--- src/server/api/config.ts
+++ src/server/api/config.ts
@@ -1,4 +1,7 @@
+const isDev = process.env.NODE_ENV === "development"
+const uri = process.env.NODE_ENV === "development" ? "http://192.168.40.156:3000/api" : "http://www.liuyouren.top/api"
 export default {
-    baseURL: "http://localhost:3000/api"
+    // baseURL: "http://localhost:3000/api"
+    baseURL: uri
     // baseURL: "http://www.liuyouren.top/api"
 }
