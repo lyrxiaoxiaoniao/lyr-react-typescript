0bc2e56edce80d786cafe1670bb8b5efb95885ef
liuyouren <lyr632537786@163.com>
2019-06-20 11:44:15 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -13,9 +13,9 @@ import App from "@shared/App"
 import * as store from "./store"
 // 解决模块声明问题 这时候我们在根目录下新建一个@types文件夹，用于存放.scss的模块声明，以及后续需要用到的全局校验接口，然后新建typed-css-modules.d.ts文件用于存放.scss模块声明
 // import * as styles from './index.scss';
-const browserHistory = createBrowserHistory()
-// const hashHistory = createHashHistory()
-const history = syncHistoryWithStore(browserHistory, store.routerStore)
+// const browserHistory = createBrowserHistory()
+const hashHistory = createHashHistory()
+const history = syncHistoryWithStore(hashHistory, store.routerStore)
 
 // 这里面的configure({enforceActions: 'observed'})
 // 用于限制被observable(也就是store中添加了@observable)的数据的修改方式，让其只能添加了@action的函数中进行修改。
